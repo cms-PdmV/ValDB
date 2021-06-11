b7175abb8b6d96ac52fed967f2e498927bd6bb47
Justinas Rumševičius <justinasr@users.noreply.github.com>
2021-06-11 13:41:01 +0200

    Update valdb_backup.py

--- valdb_backup.py
+++ valdb_backup.py
@@ -50,175 +50,169 @@ username = secrets['PdmV/valdb']['connections']['dev']['writer']['user']
 password = secrets['PdmV/valdb']['connections']['dev']['writer']['password']
 
 dsn_tns = cx_Oracle.makedsn(host_name, port_number, service_name=service_name)
-conn = cx_Oracle.connect(user=username, password=password, dsn=dsn_tns)
-
-cursor = conn.cursor()
-print('Fetching releases')
-releases = [create_release(r) for r in cursor.execute('select * from CMS_PDMV_VAL.releases')]
-print('Fetching releases_lv')
-releases_lv = [create_release(r) for r in cursor.execute('select * from CMS_PDMV_VAL.releases_lv')]
-print('Fetching status')
-status = [create_status(r) for r in cursor.execute('select * from CMS_PDMV_VAL.status')]
-print('Fetching status_lv')
-status_lv = [create_status(r) for r in cursor.execute('select * from CMS_PDMV_VAL.status_lv')]
-print('Fetching users')
-users = [create_user(r) for r in cursor.execute('select * from CMS_PDMV_VAL.users')]
-print('Fetching user rights')
-user_rights = [create_user_rights(r) for r in cursor.execute('select * from CMS_PDMV_VAL.user_rights')]
-
-conn.close()
-
-print('Fetched %s releases' % (len(releases)))
-print('Fetched %s releases_lv' % (len(releases_lv)))
-print('Fetched %s status' % (len(status)))
-print('Fetched %s status_lv' % (len(status_lv)))
-print('Fetched %s users' % (len(users)))
-print('Fetched %s user_rights' % (len(user_rights)))
+valdb_conn = cx_Oracle.connect(user=username, password=password, dsn=dsn_tns)
+valdb_cursor = valdb_conn.cursor()
 
 backup_name = sys.argv[1]
-conn = sqlite3.connect(backup_name)
-cursor = conn.cursor()
+backup_conn = sqlite3.connect(backup_name)
+backup_cursor = backup_conn.cursor()
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS releases
-                  (id integer PRIMARY KEY,
-                   category text,
-                   subcategory text,
-                   release_name text,
-                   version integer,
-                   date integer,
-                   status_kind text)''')
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS releases_lv
-                  (id integer PRIMARY KEY,
-                   category text,
-                   subcategory text,
-                   release_name text,
-                   version integer,
-                   date integer,
-                   status_kind text)''')
+backup_cursor.execute('''CREATE TABLE IF NOT EXISTS releases
+                         (id integer PRIMARY KEY,
+                          category text,
+                          subcategory text,
+                          release_name text,
+                          version integer,
+                          date integer,
+                          status_kind text)''')
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS status
-                  (id integer,
-                   validation_status text,
-                   comments text,
-                   links text,
-                   user_name text,
-                   messageID text,
-                   email_subject text,
-                   relmon_url text,
-                   FOREIGN KEY(id) REFERENCES releases(id))''')
+backup_cursor.execute('''CREATE TABLE IF NOT EXISTS releases_lv
+                         (id integer PRIMARY KEY,
+                          category text,
+                          subcategory text,
+                          release_name text,
+                          version integer,
+                          date integer,
+                          status_kind text)''')
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS status_lv
-                  (id integer,
-                   validation_status text,
-                   comments text,
-                   links text,
-                   user_name text,
-                   messageID text,
-                   email_subject text,
-                   relmon_url text,
-                   FOREIGN KEY(id) REFERENCES releases(id))''')
+backup_cursor.execute('''CREATE TABLE IF NOT EXISTS status
+                         (id integer,
+                          validation_status text,
+                          comments text,
+                          links text,
+                          user_name text,
+                          messageID text,
+                          email_subject text,
+                          relmon_url text,
+                          FOREIGN KEY(id) REFERENCES releases(id))''')
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS users
-                  (user_name text,
-                   email text,
-                   admin short,
-                   validator short)''')
+backup_cursor.execute('''CREATE TABLE IF NOT EXISTS status_lv
+                         (id integer,
+                          validation_status text,
+                          comments text,
+                          links text,
+                          user_name text,
+                          messageID text,
+                          email_subject text,
+                          relmon_url text,
+                          FOREIGN KEY(id) REFERENCES releases(id))''')
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS user_rights
-                  (id integer PRIMARY KEY,
-                   user_name text,
-                   category text,
-                   subcategory text,
-                   status_kind text)''')
+backup_cursor.execute('''CREATE TABLE IF NOT EXISTS users
+                         (user_name text,
+                          email text,
+                          admin short,
+                          validator short)''')
 
-conn.commit();
-cursor.execute('DELETE FROM releases;')
-cursor.execute('DELETE FROM releases_lv;')
-cursor.execute('DELETE FROM status;')
-cursor.execute('DELETE FROM status_lv;')
-cursor.execute('DELETE FROM users;')
-cursor.execute('DELETE FROM user_rights;')
-conn.commit();
+backup_cursor.execute('''CREATE TABLE IF NOT EXISTS user_rights
+                         (id integer PRIMARY KEY,
+                          user_name text,
+                          category text,
+                          subcategory text,
+                          status_kind text)''')
 
-for i, release in enumerate(releases):
-    cursor.execute('INSERT INTO releases VALUES (?, ?, ?, ?, ?, ?, ?)', [release['id'],
-                                                                         release['category'],
-                                                                         release['subcategory'],
-                                                                         release['release_name'],
-                                                                         release['version'],
-                                                                         release['date'],
-                                                                         release['status_kind']])
-    if i % 1000 == 0:
-        print('Commit releases after %s insertions' % (i))
-        conn.commit()
+backup_conn.commit();
+backup_cursor.execute('DELETE FROM releases;')
+backup_cursor.execute('DELETE FROM releases_lv;')
+backup_cursor.execute('DELETE FROM status;')
+backup_cursor.execute('DELETE FROM status_lv;')
+backup_cursor.execute('DELETE FROM users;')
+backup_cursor.execute('DELETE FROM user_rights;')
+backup_conn.commit();
 
-conn.commit()
+print('Fetching releases')
+for i, r in enumerate(valdb_cursor.execute('select * from CMS_PDMV_VAL.releases')):
+    release = create_release(r)
+    backup_cursor.execute('INSERT INTO releases VALUES (?, ?, ?, ?, ?, ?, ?)', [release['id'],
+                                                                                release['category'],
+                                                                                release['subcategory'],
+                                                                                release['release_name'],
+                                                                                release['version'],
+                                                                                release['date'],
+                                                                                release['status_kind']])
+    if i and i % 1000 == 0:
+        print('Commiting releases after %s insertions' % (i))
+        backup_conn.commit()
 
-for i, release in enumerate(releases_lv):
-    cursor.execute('INSERT INTO releases_lv VALUES (?, ?, ?, ?, ?, ?, ?)', [release['id'],
-                                                                            release['category'],
-                                                                            release['subcategory'],
-                                                                            release['release_name'],
-                                                                            release['version'],
-                                                                            release['date'],
-                                                                            release['status_kind']])
-    if i % 1000 == 0:
-        print('Commit releases_lv after %s insertions' % (i))
-        conn.commit()
+backup_conn.commit()
 
-conn.commit()
+print('Fetching releases_lv')
+for i, r in enumerate(valdb_cursor.execute('select * from CMS_PDMV_VAL.releases_lv')):
+    release = create_release(r)
+    backup_cursor.execute('INSERT INTO releases_lv VALUES (?, ?, ?, ?, ?, ?, ?)', [release['id'],
+                                                                                   release['category'],
+                                                                                   release['subcategory'],
+                                                                                   release['release_name'],
+                                                                                   release['version'],
+                                                                                   release['date'],
+                                                                                   release['status_kind']])
+    if i and i % 1000 == 0:
+        print('Commiting releases_lv after %s insertions' % (i))
+        backup_conn.commit()
 
-for i, stat in enumerate(status):
-    cursor.execute('INSERT INTO status VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [stat['id'],
-                                                                          stat['validation_status'],
-                                                                          stat['comments'],
-                                                                          stat['links'],
-                                                                          stat['user_name'],
-                                                                          stat['messageID'],
-                                                                          stat['email_subject'],
-                                                                          stat['relmon_url']])
-    if i % 1000 == 0:
-        print('Commit status after %s insertions' % (i))
-        conn.commit()
+backup_conn.commit()
 
-conn.commit()
+print('Fetching status')
+for i, r in enumerate(valdb_cursor.execute('select * from CMS_PDMV_VAL.status')):
+    status = create_status(r)
+    backup_cursor.execute('INSERT INTO status VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [status['id'],
+                                                                                 status['validation_status'],
+                                                                                 status['comments'],
+                                                                                 status['links'],
+                                                                                 status['user_name'],
+                                                                                 status['messageID'],
+                                                                                 status['email_subject'],
+                                                                                 status['relmon_url']])
+    if i and i % 1000 == 0:
+        print('Commiting status after %s insertions' % (i))
+        backup_conn.commit()
 
-for i, stat in enumerate(status_lv):
-    cursor.execute('INSERT INTO status_lv VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [stat['id'],
-                                                                             stat['validation_status'],
-                                                                             stat['comments'],
-                                                                             stat['links'],
-                                                                             stat['user_name'],
-                                                                             stat['messageID'],
-                                                                             stat['email_subject'],
-                                                                             stat['relmon_url']])
-    if i % 1000 == 0:
-        print('Commit status_lv after %s insertions' % (i))
-        conn.commit()
+backup_conn.commit()
 
-conn.commit()
+print('Fetching status_lv')
+for r in valdb_cursor.execute('select * from CMS_PDMV_VAL.status_lv'):
+    status  = create_status(r)
+    backup_cursor.execute('INSERT INTO status_lv VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [status['id'],
+                                                                                    status['validation_status'],
+                                                                                    status['comments'],
+                                                                                    status['links'],
+                                                                                    status['user_name'],
+                                                                                    status['messageID'],
+                                                                                    status['email_subject'],
+                                                                                    status['relmon_url']])
+    if i and i % 1000 == 0:
+        print('Commiting status_lv after %s insertions' % (i))
+        backup_conn.commit()
 
-for i, user in enumerate(users):
-    cursor.execute('INSERT INTO users VALUES (?, ?, ?, ?)', [user['user_name'],
-                                                             user['email'],
-                                                             user['admin'],
-                                                             user['validator']])
-    if i % 1000 == 0:
-        print('Commit users after %s insertions' % (i))
-        conn.commit()
+backup_conn.commit()
 
-conn.commit()
+print('Fetching users')
+for r in valdb_cursor.execute('select * from CMS_PDMV_VAL.users'):
+    user = create_user(r)
+    backup_cursor.execute('INSERT INTO users VALUES (?, ?, ?, ?)', [user['user_name'],
+                                                                    user['email'],
+                                                                    user['admin'],
+                                                                    user['validator']])
+    if i and i % 1000 == 0:
+        print('Commiting users after %s insertions' % (i))
+        backup_conn.commit()
 
-for i, user_right in enumerate(user_rights):
-    cursor.execute('INSERT INTO user_rights VALUES (?, ?, ?, ?, ?)', [user_right['id'],
-                                                                      user_right['user_name'],
-                                                                      user_right['category'],
-                                                                      user_right['subcategory'],
-                                                                      user_right['status_kind']])
-    if i % 1000 == 0:
-        print('Commit user_rights after %s insertions' % (i))
-        conn.commit()
+backup_conn.commit()
 
-conn.commit()
-conn.close()
+print('Fetching user rights')
+for r in valdb_cursor.execute('select * from CMS_PDMV_VAL.user_rights'):
+    user_right = create_user_rights(r)
+    backup_cursor.execute('INSERT INTO user_rights VALUES (?, ?, ?, ?, ?)', [user_right['id'],
+                                                                             user_right['user_name'],
+                                                                             user_right['category'],
+                                                                             user_right['subcategory'],
+                                                                             user_right['status_kind']])
+    if i and i % 1000 == 0:
+        print('Commiting user_rights after %s insertions' % (i))
+        backup_conn.commit()
+
+backup_conn.commit()
+
+valdb_conn.close()
+
+backup_conn.close()
